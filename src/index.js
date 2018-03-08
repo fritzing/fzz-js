54c8f24a45d2874eae4f9dc94d647affafda3b0e
Paul Vollmer <paul.vollmer@protonmail.com>
2018-03-08 08:53:14 +0100

    Move index.js to src dir
    Then we can compile the source with the make build task

---
+++ src/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const FZBoard = require('./fz/board');
+const FZConnector = require('./fz/connector');
+const FZ = require('./fz/fz');
+const FZGeometry = require('./fz/geometry');
+const FZInstance = require('./fz/instance');
+const FZLeg = require('./fz/leg');
+const FZProgram = require('./fz/program');
+const Vector2D = require('./fz/vector2d');
+const FZZ = require('./fzz/fzz');
+const FZZCode = require('./fzz/code');
+const FZZUtils = require('./utils');
+
+module.exports = {
+  FZBoard,
+  FZConnector,
+  FZ,
+  FZGeometry,
+  FZInstance,
+  FZLeg,
+  FZProgram,
+  Vector2D,
+  FZZ,
+  FZZCode,
+  FZZUtils,
+};
